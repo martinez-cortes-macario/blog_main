4f3db2431bed2438ffd5451e0a1d581dc9b0c4b0
martinez-cortes-macario <m23796376@gmail.com>
2026-03-23 15:28:56 -0700

    Actualizacion del admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Post, Category, Comment
 
 
-
 class CommentItemInline(admin.TabularInline):
     model = Comment
     raw_id_fields = ['post']
+
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'intro', 'body']
@@ -13,13 +13,15 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['category', 'created_at', 'status']
     inlines = [CommentItemInline]
 
+
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['title']
-    list_display = ('title')
+    list_display = ('title',)  
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'post', 'created_at')
-    
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
