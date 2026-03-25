9196b71320ba7853f0b9ac9560c1f8a5c3c5bb94
martinez-cortes-macario <m23796376@gmail.com>
2026-03-24 17:17:45 -0700

    Html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8-*-
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render
+
 from .models import Post, Category
 
 # Create your views here.
@@ -9,3 +10,12 @@ def home(request):
         'posts': posts
     }
     return render(request, 'blog/home.html', context)
+
+def detail(request, id):
+    post = get_object_or_404(Post, id = id, status=Post.ACTIVE)
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/detail.html', context)
+
