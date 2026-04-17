73659b224bd861f7098225fb5b28fd3f591dd1e6
martinez-cortes-macario <m23796376@gmail.com>
2026-04-16 17:34:33 -0700

    Actualizacion 16/04/2026

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,36 @@
 #-*- coding: utf-8-*-
-from django.shortcuts import render, get_object_or_404, render
+
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post, Category
+from .forms import CommentForm
 
 # Create your views here.
 def home(request):
+
     posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
     context = {
         'posts': posts
     }
     return render(request, 'blog/home.html', context)
-
 def detail(request, id):
     post = get_object_or_404(Post, id = id, status=Post.ACTIVE)
+    
+    if request.method =='POST':
+        form = CommentForm (request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', id=id)
+    else:
+            form = CommentForm()
 
     context = {
         'post': post,
+        'form': form
     }
     return render(request, 'blog/detail.html', context)
 
